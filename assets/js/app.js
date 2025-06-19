ee96c1ef23d521cb93bc1149197a88c53d408c07
unknown <ajay2003bishnoi@gmail.com>
2025-06-19 11:45:29 +0530

    changes added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,14 @@
 "use strict";
+
+const measureKelvin = function() {
+    const measurement = {
+        type: "temperature",
+        unit: "celsius",
+        value: prompt("Degrees Celsius:"),
+    }
+    
+    const kelvin = `Measurement value ${measurement.value} + 273 : ${measurement.value + 273}`;
+    return kelvin;
+}
+
+console.log(`${measureKelvin()}`);
